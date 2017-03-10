bfcc1b940d5d6e7d29433a0de719b515b4eb462e
Josep Otal <j.otal.pares@gmail.com>
2017-03-10 12:47:40 +0100

    angular working need to refactor

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,38 +1,38 @@
-/* Graph */
-let myData = [ 'data1'/*, 12, 34, 45 */];
-//let myAvg = ['avg']
-let myAxis = ['x'/*,'2013/01/01', '2013/01/02', '2013/01/03'*/]
-const chart = c3.generate({
-  bindto: '#chart',
-  data: {
-    x: 'x',
-    columns: [
-      myAxis,
-      myData
-    ]
-  },
-  axis: {
-    x: {
-      type: 'timeseries',
-      tick: {
-        format: '%H:%M:%S'
-      }
-    }
-  }
-});
+// /* Graph */
+// let myData = [ 'data1'/*, 12, 34, 45 */];
+// //let myAvg = ['avg']
+// let myAxis = ['x'/*,'2013/01/01', '2013/01/02', '2013/01/03'*/]
+// const chart = c3.generate({
+//   bindto: '#chart',
+//   data: {
+//     x: 'x',
+//     columns: [
+//       myAxis,
+//       myData
+//     ]
+//   },
+//   axis: {
+//     x: {
+//       type: 'timeseries',
+//       tick: {
+//         format: '%H:%M:%S'
+//       }
+//     }
+//   }
+// });
 
 
-var socket = io.connect()
-  socket.on('new read', function(data) {
-    //the list
-    //const oList = document.getElementById('list')
-    //const html = oList.innerHTML
-    const value = data.current
-    const date = Date.now()
-    //oList.innerHTML = html + `<li>${value}</li>`
-    myData.push( +value )
-    myAxis.push( date )
-    console.log(myData)
-    console.log(myAxis)
-    chart.load({ columns: [myAxis, myData] })
-  })
+// var socket = io.connect()
+//   socket.on('new read', function(data) {
+//     //the list
+//     //const oList = document.getElementById('list')
+//     //const html = oList.innerHTML
+//     const value = data.current
+//     const date = Date.now()
+//     //oList.innerHTML = html + `<li>${value}</li>`
+//     myData.push( +value )
+//     myAxis.push( date )
+//     console.log(myData)
+//     console.log(myAxis)
+//     chart.load({ columns: [myAxis, myData] })
+//   })
