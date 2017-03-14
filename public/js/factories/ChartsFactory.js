3109749c9d0f0a2fe12399a36f90684198382d64
Josep Otal <j.otal.pares@gmail.com>
2017-03-14 10:38:29 +0100

    charts updated

--- public/js/factories/ChartsFactory.js
+++ public/js/factories/ChartsFactory.js
@@ -15,83 +15,84 @@
 
         return service;
 
+
+
+
         ///////mehthods from the factory
         
-        function getChartNow () {
-          /// CHART NOW-------------------------------------------------
-          Highcharts.setOptions({
-                  global: {
-                      useUTC: false
-                  }
-              });
+              function getChartNow () {
 
-          Highcharts.chart('chart-now', {
-              chart: {
-                  type: 'spline',
-                  animation: Highcharts.svg, // don't animate in old IE
-                  marginRight: 10,
-                  events: {
-                      load: function () {
-                          var series = this.series[0]
-                          var socket = io.connect()
-                          socket.on('new read', function(data) {
-                              var x = (new Date()).getTime(), // current time
-                                  y = data.current;
-                              series.addPoint([x, y], true, true);
-                          });
-                      }
-                  }
-              },
+                Highcharts.setOptions({
+          global: {
+              useUTC: false
+          }
+      });
 
-              title: {
-                  text: 'Live random data'
-              },
-              xAxis: {
-                  type: 'datetime',
-                  tickPixelInterval: 150
-              },
-              yAxis: {
-                  title: {
-                      text: 'Value'
-                  },
-                  plotLines: [{
-                      value: 0,
-                      width: 1,
-                      color: '#808080'
-                  }]
-              },
-              tooltip: {
-                  formatter: function () {
-                      return '<b>' + this.series.name + '</b><br/>' +
-                          Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                          Highcharts.numberFormat(this.y, 2);
+      // Create the chart
+      Highcharts.stockChart('chart-now', {
+          chart: {
+              events: {
+                  load: function () {
+                    var series = this.series[0]
+                    var socket = io.connect()
+                    socket.on('new read', function(data) {
+                        var x = (new Date()).getTime(), // current time
+                            y = data.current;
+                        series.addPoint([x, y], true, true);
+                    });
                   }
-              },
-              legend: {
-                  enabled: false
-              },
-              exporting: {
-                  enabled: false
-              },
-              series: [{
-                  name: 'Random data',
-                  data: (function () {
-                      // generate an array of random data, fixed
-                      var data = [],
-                          time = (new Date()).getTime(),
-                          i;
+              }
+          },
 
-                      for (i = -19; i <= 0; i += 1) {
-                          data.push({
-                              x: time + i * 1000,
-                              y: data.current
-                          });
-                      }
-                      return data;
-                  }())
-              }]
-          });
-        }
+          rangeSelector: {
+              buttons: [{
+                  count: 1,
+                  type: 'minute',
+                  text: '1M'
+              }, {
+                  count: 5,
+                  type: 'minute',
+                  text: '5M'
+              }, {
+                  type: 'all',
+                  text: 'All'
+              }],
+              inputEnabled: false,
+              selected: 0
+          },
+
+          title: {
+              text: 'Live random data'
+          },
+
+          exporting: {
+              enabled: false
+          },
+
+          series: [{
+              name: 'Random data',
+              data: (function () {
+                  // generate an array of random data
+                  var data = [],
+                      time = (new Date()).getTime(),
+                      i;
+
+                  for (i = -999; i <= 0; i += 1) {
+                      data.push([
+                          time + i * 1000,
+                          data.current
+                      ]);
+                  }
+                  return data;
+              }())
+          }]
+      });
+
+
+                /// CHART NOW-------------------------------------------------
+                
+                
+              }
 
         function getChartDay () {
           
@@ -185,70 +186,38 @@
 
         function getChartMonth () {
           // CHART MONTH
-            Highcharts.chart('chart-year', {
+            $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-v.json&callback=?', function (data) {
+
+    // create the chart
+            Highcharts.stockChart('chart-year', {
                 chart: {
-                    type: 'column'
+                    alignTicks: false
                 },
+
+                rangeSelector: {
+                    selected: 1
+                },
+
                 title: {
-                    text: 'Monthly Average Rainfall'
+                    text: 'AAPL Stock Volume'
                 },
-                subtitle: {
-                    text: 'Source: WorldClimate.com'
-                },
-                xAxis: {
-                    categories: [
-                        'Jan','Feb',
-                        'Mar',
-                        'Apr',
-                        'May',
-                        'Jun',
-                        'Jul',
-                        'Aug',
-                        'Sep',
-                        'Oct',
-                        'Nov',
-                        'Dec'
-                    ],
-                    crosshair: true
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Rainfall (mm)'
-                    }
-                },
-                tooltip: {
-                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-                    footerFormat: '</table>',
-                    shared: true,
-                    useHTML: true
-                },
-                plotOptions: {
-                    column: {
-                        pointPadding: 0.2,
-                        borderWidth: 0
-                    }
-                },
+
                 series: [{
-                    name: 'Tokyo',
-                    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-                }, {
-                    name: 'New York',
-                    data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-                }, {
-                    name: 'London',
-                    data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-                }, {
-                    name: 'Berlin',
-                    data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
+                    type: 'column',
+                    name: 'AAPL Stock Volume',
+                    data: data,
+                    dataGrouping: {
+                        units: [[
+                            'week', // unit name
+                            [1] // allowed multiples
+                        ], [
+                            'month',
+                            [1, 2, 3, 4, 6]
+                        ]]
+                    }
                 }]
             });
+        });
         }
     }
 })()
