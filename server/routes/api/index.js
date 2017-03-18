7772d4d9fd08871ec1655caa5fcab64458463a95
Josep Otal <j.otal.pares@gmail.com>
2017-03-18 14:34:57 +0100

    new form template

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,11 +3,11 @@ const router = express.Router()
 
 const getAll = require('./handlers/getAll')
 const getUser = require('./handlers/getUser')
-//const updateUser = require('./handlers/updateUser')
+const updateUser = require('./handlers/updateUser')
 
 router.get('/users', getAll)
 router.get('/users/:id', getUser)
 
-//router.put('/updateUser', updateUser)
+router.put('/updateUser', updateUser)
 
 module.exports = router
