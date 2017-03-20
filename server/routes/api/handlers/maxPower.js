2339d4181427adf9ab80482cf6b705f987965ced
Josep Otal <j.otal.pares@gmail.com>
2017-03-20 19:09:40 +0100

    form and max power working fine

--- server/routes/api/handlers/maxPower.js
+++ server/routes/api/handlers/maxPower.js
@@ -4,11 +4,11 @@ module.exports = (req,res) => {
 
   let id  = req.params.id
   let {maxPower} = req.body
-  console.log(maxPower)
+  //console.log(maxPower)
 
   User.findByIdAndUpdate( id,  { maxPower } )
     .then( user => {
-      console.log('user has been updated succesfully')
+      //console.log('user has been updated succesfully')
       res.status(200).json(user)
     })
     .catch( err => res.status(500).json(err) )
