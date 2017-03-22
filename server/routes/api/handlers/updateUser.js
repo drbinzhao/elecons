3ac4f832437c0a83e09bf00633b30c9de2e0b405
Josep Otal <j.otal.pares@gmail.com>
2017-03-23 00:38:55 +0100

    fixed bug null consumption when save data

--- server/routes/api/handlers/updateUser.js
+++ server/routes/api/handlers/updateUser.js
@@ -3,10 +3,10 @@ const User = require( __base + 'models/User')
 module.exports = (req,res) => {
 
   const  id  = req.params.id
-  let { firstName, lastName, email, contractedPower, energyTariff, urlCurrentPower, consumption2016, consumption2017 } = req.body
+  let { firstName, lastName, email, contractedPower, energyTariff, urlCurrentPower} = req.body
   const updatedAt = Date.now()
   
-  User.findByIdAndUpdate( id,  { firstName, lastName, email, contractedPower, energyTariff, updatedAt, urlCurrentPower, consumption2016, consumption2017 } )
+  User.findByIdAndUpdate( id,  { firstName, lastName, email, contractedPower, energyTariff, updatedAt, urlCurrentPower } )
     .then( user => {
       console.log('user has been updated succesfully')
       res.status(200).json(user)
