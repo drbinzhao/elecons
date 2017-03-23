47f01d893d028072567bb91d979c0d17fe921238
Josep Otal <j.otal.pares@gmail.com>
2017-03-23 23:42:26 +0100

    restore run

--- client/js/run.js
+++ client/js/run.js
@@ -29,12 +29,12 @@ angular.module('EleconsApp')
                 $rootScope.updatedAt = response.data.updatedAt 
                 $rootScope.urlCurrentPower = response.data.urlCurrentPower || "http://fran.noip.me:8888/consumo?id=0001"                
             })
-          // ApiFactory.getData() 
-          //   .then (response => {
-          //       $rootScope.consumption2016 = Number(response.data.monthly[0])
-          //       $rootScope.consumption2017 = Number(response.data.monthly[1])
-          //       $rootScope.savingsKWH = Number(response.data.monthly[0]- response.data.monthly[1])
-          //       $rootScope.savingsEuro = $rootScope.savingsEuro  || ($rootScope.savingsKWH * 0.11)
-          //     })
+          ApiFactory.getData() 
+            .then (response => {
+                $rootScope.consumption2016 = Number(response.data.monthly[0])
+                $rootScope.consumption2017 = Number(response.data.monthly[1])
+                $rootScope.savingsKWH = Number(response.data.monthly[0]- response.data.monthly[1])
+                $rootScope.savingsEuro = $rootScope.savingsEuro  || ($rootScope.savingsKWH * 0.11)
+              })
       })
   })
